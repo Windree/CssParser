bf5b743cf00f3d0f94b325815c89e780207f4c4c
Windree <windree@dragonsoul.com>
2024-11-05 17:53:36 +0300

    feat: add no comment and unclosed comment tests

--- Windree.CssParser.Tests/UnitTest1.cs
+++ Windree.CssParser.Tests/UnitTest1.cs
@@ -6,11 +6,25 @@ namespace Windree.CssParser.Tests
     public class UnitTest1
     {
         [TestMethod]
+        public void TestNoComment()
+        {
+            var parser = new CssParser("  .div {} ");
+            var css = parser.Parse();
+            Assert.AreEqual(0, css.Length);
+        }
+        [TestMethod]
         public void TestComment()
         {
             var parser = new CssParser("    /*Comment*/    ");
             var css = parser.Parse();
             Assert.AreEqual("Comment", css[0].Comment);
+        }
+        [TestMethod]
+        public void TestUnclosedComment()
+        {
+            var parser = new CssParser("    /*Comment/    ");
+            var css = parser.Parse();
+            Assert.AreEqual("Comment/    ", css[0].Comment);
         }
     }
 }
